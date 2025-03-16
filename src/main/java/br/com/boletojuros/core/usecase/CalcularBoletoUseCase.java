cbf5a2bfda59858a63cbc0d1abfaf82a6a2a3c4c
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-03-16 20:00:51 -0300

    continação da regra de negócio

--- src/main/java/br/com/boletojuros/core/usecase/CalcularBoletoUseCase.java
+++ src/main/java/br/com/boletojuros/core/usecase/CalcularBoletoUseCase.java
@@ -7,32 +7,52 @@ import br.com.boletojuros.core.domain.enuns.TipoExecao;
 import br.com.boletojuros.core.exceptions.ApplicationException;
 import br.com.boletojuros.core.port.in.CalculoBoletoPort;
 import br.com.boletojuros.core.port.out.ComplementoBoletoPort;
+import br.com.boletojuros.core.port.out.SalvarCalculoBoletoPort;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Service
 public class CalcularBoletoUseCase implements CalculoBoletoPort {
 
-
+    private static final BigDecimal JUROS_DIARIO = BigDecimal.valueOf(0.033);
     private final ComplementoBoletoPort complementoBoletoPort;
+    private final SalvarCalculoBoletoPort salvarCalculoBoletoPort;
 
-    public CalcularBoletoUseCase(ComplementoBoletoPort complementoBoletoPort) {
+    public CalcularBoletoUseCase(ComplementoBoletoPort complementoBoletoPort,
+                                 SalvarCalculoBoletoPort salvarCalculoBoletoPort) {
         this.complementoBoletoPort = complementoBoletoPort;
+        this.salvarCalculoBoletoPort = salvarCalculoBoletoPort;
     }
 
     @Override
     public BoletoCalculado executar(String codigo, LocalDate dataPagamento) {
         var boleto = complementoBoletoPort.executar(codigo);
-        validar(boleto);
 
         // TODO - validar boleto
+        validar(boleto);
 
         //TODO - calcular boleto
+        var diasVencidos = getDiasVencidos(boleto.getDataVencimento(), dataPagamento);
+        var valorJurosDias = JUROS_DIARIO.multiply(boleto.getValor().divide(BigDecimal.valueOf(100)));
+        var juros = valorJurosDias.multiply(BigDecimal.valueOf(diasVencidos)).setScale(2, RoundingMode.HALF_EVEN);
+        var boletoCalculado = BoletoCalculado.builder()
+                .codigo(boleto.getCodigo())
+                .dataPagamento(dataPagamento)
+                .juros(juros)
+                .dataVencimento(boleto.getDataVencimento())
+                .valorOriginal(boleto.getValor())
+                .valor(boleto.getValor().add(juros))
+                .tipo(boleto.getTipo())
+                .build();
 
         // TODO - Salvar boleto
+        salvarCalculoBoletoPort.executar(boletoCalculado);
 
-        return null;
+        return boletoCalculado;
     }
 
     private void validar(Boleto boleto) {
@@ -47,5 +67,10 @@ public class CalcularBoletoUseCase implements CalculoBoletoPort {
         if (boleto.getDataVencimento().isAfter(LocalDate.now())) {
             throw new ApplicationException((TipoExecao.BOLETO_NAO_VENCIDO));
         }
+    }
+
+
+    private Long getDiasVencidos(LocalDate dataVencimento, LocalDate dataPagamento) {
+        ChronoUnit.DAYS.between(dataVencimento, dataPagamento);
     }
 }
