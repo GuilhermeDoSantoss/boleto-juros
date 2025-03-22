c5943c45ce9b6828359732e217cfb2a34264feed
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-03-22 01:25:36 -0300

    Iniciando a criação do adapter de saída complemento do boleto

---
+++ src/main/java/br/com/boletojuros/adapter/datasource/integration/dto/BoletoDTO.java
@@ -0,0 +1,25 @@
+package br.com.boletojuros.adapter.datasource.integration.dto;
+
+import br.com.boletojuros.core.domain.enuns.TipoBoleto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import feign.form.FormProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+
+public class BoletoDTO {
+
+    private String codigo;
+
+    @JsonProperty("data_vencimento")
+    private LocalDate dataVencimento;
+
+    private BigDecimal valor;
+
+    private TipoBoleto tipo;
+}
